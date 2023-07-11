6f65dcef888d45bab81e02b1c45da6b499e343d0
antonio.santese <antonio.santese@cetma.it>
2023-07-11 12:04:06 +0200

    developed

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,76 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "first-launch-reminder" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('first-launch-reminder.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from First Launch Reminder!');
+	// Retrieve the message from the global state, or use a default message if there is no saved message
+	let message = context.globalState.get<string>('message') || 'Do not forget to do X!';
+
+	// Create and show a new Webview when the extension is activated
+	const panel = vscode.window.createWebviewPanel('reminder', 'Reminder', vscode.ViewColumn.One, {
+		// Enable scripts in the webview
+		enableScripts: true,
 	});
 
-	context.subscriptions.push(disposable);
+	panel.webview.html = getWebviewContent(message);
+
+	// Listen to messages sent from the webview
+	panel.webview.onDidReceiveMessage(async message => {
+		if (message.command === 'updateMessage') {
+			const newMessage = await vscode.window.showInputBox({ prompt: 'Enter a new message' });
+			if (newMessage !== undefined) {
+				// Save the new message in the global state
+				await context.globalState.update('message', newMessage);
+				// Update the webview with the new message
+				panel.webview.html = getWebviewContent(newMessage);
+			}
+		}
+	}, undefined, context.subscriptions);
+
+	context.subscriptions.push(panel);
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+function getWebviewContent(message: string) {
+	// Use the provided message as the content of the webview
+	return `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Reminder</title>
+            <style>
+                body {
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    flex-direction: column;
+                    height: 100vh;
+                    font-size: 2em;
+                    font-family: Arial, sans-serif;
+                }
+                button {
+                    margin-top: 2em;
+                    padding: 1em 2em;
+                    font-size: 0.5em;
+                }
+            </style>
+        </head>
+        <body>
+            <div>
+                ${message}
+            </div>
+            <button id="updateButton">Update Message</button>
+            <script>
+                const vscode = acquireVsCodeApi();
+                document.getElementById('updateButton').addEventListener('click', () => {
+                    vscode.postMessage({ command: 'updateMessage' });
+                });
+            </script>
+        </body>
+        </html>
+    `;
+}
+
+export function deactivate() { }
